4c701c3bc40f2841b7052f7d2460f43132a8c64a
yusbel <yusbel14sj@gmail.com>
2023-12-04 22:17:45 -0500

    lost add

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ const regex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
 
 inputEmail.addEventListener('change',()=> {
     if(!regex.test(inputEmail.value)){
-        alert('es falso');
-    //    inputEmail.classList.add('.frm-cnt__correo--invalid');
+        inputEmail.classList.add('frm-cnt__correo--invalid');
+        btn.classList.remove('frm-cnt__btn--valid');
         return
     }else{
-        alert('es true')
-    //    btn.classList.add('.frm-cnt__correo--valid');
+        btn.classList.add('frm-cnt__btn--valid');
         return
     }
 })
